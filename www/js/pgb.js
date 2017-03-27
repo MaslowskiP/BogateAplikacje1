5e5d6e345f1110dd5588ecca8c90c76327b94558
MaslowskiP <ziem94@gmail.com>
2017-03-27 16:14:02 +0200

    poprawienie literowki

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -1,10 +1,11 @@
 function init() {
-	navigator.splashscreen.show();
+	
 	document.addEventListener("deviceready",onDeviceReady, false);
 }
 
 function onDeviceReady() {
 	navigator.notification.beep(1);
+	navigator.splashscreen.show();
 }
 
 function deviceInfo() {
