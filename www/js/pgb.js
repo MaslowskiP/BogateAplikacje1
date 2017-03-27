1a196c66a1d1edcef83eb049b535ab0345360010
MaslowskiP <ziem94@gmail.com>
2017-03-27 15:11:43 +0200

    Przerobienie przykładowej funkcji pakietu na język polski

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -32,14 +32,14 @@ function checkConnection() {
     var networkState = navigator.connection.type;
 
     var states = {};
-    states[Connection.UNKNOWN]  = 'Unknown connection';
-    states[Connection.ETHERNET] = 'Ethernet connection';
-    states[Connection.WIFI]     = 'WiFi connection';
-    states[Connection.CELL_2G]  = 'Cell 2G connection';
-    states[Connection.CELL_3G]  = 'Cell 3G connection';
-    states[Connection.CELL_4G]  = 'Cell 4G connection';
-    states[Connection.CELL]     = 'Cell generic connection';
-    states[Connection.NONE]     = 'No network connection';
+    states[Connection.UNKNOWN]  = 'Nieznane połączenie';
+    states[Connection.ETHERNET] = 'Połaczenie Ethernet';
+    states[Connection.WIFI]     = 'Połączenie WiFi';
+    states[Connection.CELL_2G]  = 'Telefoniczna sieć 2G';
+    states[Connection.CELL_3G]  = 'Telefoniczna sieć 3G';
+    states[Connection.CELL_4G]  = 'Telefoniczna sieć 4G';
+    states[Connection.CELL]     = 'Telefoniczna sieć';
+    states[Connection.NONE]     = 'Brak połączenia';
 
     navigator.notification.alert('Connection type: ' + states[networkState]);
 }
